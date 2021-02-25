a4f6de8aff1e657c33b20e413c816b8cbb09281e
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-25 17:38:32 -0500

    commit tree 576687c05f265628e0459319de5bb09aeae0b927

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+// let formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle"
+// };
+
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   }, 
+//   body: JSON.stringify(formData)
+// };
+
+// fetch('http://localhost:3000/dogs', configObj)
+//   .then(function(resp){
+//     return resp.json();
+//   })
+//   .then(function(obj){
+//     console.log(obj)
+//   })
+//   .catch(function(error){
+//     alert('Bad things! Ragnarôk!');
+//     console.log(error.message)
+//   })
+
+  function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email
+        })
+      })
+      .then(function(resp){
+        return resp.json();
+      })
+      .then(function(obj){
+        document.body.innerHTML = obj['id']
+      })
+      .catch(function(error){
+        document.body.innerHTML = error.message
+      })
+  }
+      
